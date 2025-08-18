2b9c924c11a7bc70ee092a2ccff0c0bef7359716
Donk <dkdanjolly@gmail.com>
2025-08-18 17:58:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ CertLink.onmouseover = () => {
 };
 CertLink.onmouseout = () => document.getElementById('hover-text')?.remove();
 document.documentElement.style.scrollBehavior = "smooth";
-document.documentElement.style.scrollPaddingTop = "80px";
+document.documentElement.style.scrollPaddingTop = "120px";
+
 
 
